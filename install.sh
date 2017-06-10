e1d58c3d9c0791f098e1ad34e42b63f3a7891a12
Matthew Larson <theguy@gurumojo.net>
2017-06-10 12:28:13 -0700

    install.sh with file based libraries

--- install.sh
+++ install.sh
@@ -5,11 +5,18 @@ npm install yarn &> /dev/null
 
 echo "Installing dependencies ..."
 yarn
-for i in `ls library` ;do
-	echo "library/$i ..."
-	cd library/$i
-	yarn
-	cd - 1&>/dev/null
+for lib in `ls library` ;do
+	# isolate name from file extension (if any)
+	name=$(echo "$lib" | sed -e 's;\(.*\)\.[a-z]*$;\1;')
+	# filter directories from files
+	if [ "$name" == "$lib" ] ;then
+		echo "library/$name dependencies ..."
+		cd "library/$name"
+		yarn
+		cd - 1&> /dev/null
+	else
+		echo "library/$name is not a package ... skipped"
+	fi
 done
 
 echo "Cleaning up ..."
