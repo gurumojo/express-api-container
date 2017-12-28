ee5490204016bead11493f780ec2a63f93e5af74
Matthew Larson <theguy@gurumojo.net>
2017-12-27 20:02:19 -0800

    dynamic config updates

--- library/constant/index.js
+++ library/constant/index.js
@@ -1,22 +1,50 @@
 'use strict';
+const hostname = require('os').hostname();
+
 const express = require('./express');
+const http = require('./http');
+const jwt = require('./jwt');
 const logger = require('./logger');
+const oauth = require('./oauth');
 const postgres = require('./postgres');
 const redis = require('./redis');
 
 const environment = process.env.NODE_ENV || 'development';
-const hostname = process.env.EXPRESS_HOST || 'express-api';
+const context = require(`./${environment}`);
 
-const override = require(`./${environment}`);
-
-module.exports = Object.freeze(Object.assign(
+const staticConfig = Object.assign(
 	{
 		environment,
 		hostname
 	},
 	express,
+	http,
+	jwt,
 	logger,
+	oauth,
 	postgres,
 	redis,
-	override
-));
+	context
+);
+
+const runtimeConfig = [
+	'EXPRESS_HOST',
+	'EXPRESS_PORT',
+	'JWT_CLOCK_TOLERANCE',
+	'JWT_EXPIRES_IN',
+	'JWT_MAX_AGE',
+	'JWT_SECRET',
+	'POSTGRES_HOST',
+	'POSTGRES_PASSWORD',
+	'POSTGRES_PORT',
+	'REDIS_HOST',
+	'REDIS_PASSWORD',
+	'REDIS_PORT'
+];
+
+const dynamicConfig = runtimeConfig.reduce((override, member) => {
+	override[member] = process.env[member] || staticConfig[member]
+	return override;
+}, {});
+
+module.exports = Object.freeze(Object.assign(staticConfig, dynamicConfig));
