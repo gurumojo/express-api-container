ec2dccdc7e2cfa576300f72ef26676e84c3721ca
Matthew Larson <theguy@gurumojo.net>
2018-02-23 16:20:38 -0800

    only headers from token routes

--- route/token.js
+++ route/token.js
@@ -5,15 +5,22 @@ const {passport} = require('../library/token');
 
 token.get('/',
 	passport.authenticate('jwt-refresh'),
+	//(request, response) => response.send({token: response.locals.token})
 	(request, response) => response.set({
 		'x-access-token': response.locals.token.access,
 		'x-refresh-token': response.locals.token.refresh
 	}).status(204).send()
+	//.send({token: response.locals.token})
 );
 
 token.put('/',
 	passport.authenticate('local'),
-	(request, response) => response.send({token: response.locals.token})
+	//(request, response) => response.send({token: response.locals.token})
+	(request, response) => response.set({
+		'x-access-token': response.locals.token.access,
+		'x-refresh-token': response.locals.token.refresh
+	}).status(204).send()
+	//.send({token: response.locals.token})
 );
 
 
