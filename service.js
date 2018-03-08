ca83d8f6e5b37130225fa843bd9365ed8125f00c
Matthew Larson <theguy@gurumojo.net>
2018-03-07 19:24:57 -0800

    cluster and benchmark tooling

--- service.js
+++ service.js
@@ -1,64 +1,147 @@
 'use strict';
-const bodyParser = require('body-parser');
-const {get, partial} = require('lodash');
+const service = require('cluster');
+const {find, partial} = require('lodash');
 
 const constant = require('./lib/constant');
-const discover = require('./lib/discover');
 const json = require('./lib/json');
 const logger = require('./lib/logger');
 const network = require('./lib/network');
-//const pubsub = require('./lib/pubsub');
-const router = require('./lib/router');
-const status = require('./lib/status');
-const {passport} = require('./lib/token');
+const worker = require('./worker')
 
 const host = network().reduce((value, candidate) => {
 	return candidate === '127.0.0.1' ? value : candidate;
 }, null);
 
-const namespace = `${constant.API_NAME}`;
+const config = {
+	cores: constant.CPU_COUNT,
+	host,
+	port: constant.EXPRESS_PORT
+};
+
+const whitelist = ["code","connected","exitCode","id","killed","pid","process","state","writeQueueSize"];
+
+let workers;
+
+let namespace = `${constant.API_NAME}`;
 
 
-function catchall(request, response) {
-	response.status(constant.HTTP_STATUS_METHOD_NOT_ALLOWED).send(status.methodNotAllowed);
+function api(worker) {
+	let members = Object.keys(worker);
+	//logger.debug(`${namespace}.service.api`, json.string({members}));
+	return members.reduce((object, member) => {
+		let type = typeof worker[member];
+		let value = {};
+		if (whitelist.includes(member)) {
+			switch (type) {
+				case 'function':
+					value[member] = `function ${member}(...) {...}`;
+					break;
+				case 'object':
+					value[member] = (!!worker[member] || undefined)
+						&& api(worker[member]);
+					break;
+				default: // 'boolean', 'number', 'string', 'undefined'
+					value[member] = worker[member];
+			}
+		}
+		return Object.assign(object, value);
+	}, {});
 }
 
-function delegate(channel, message) {
-	const object = json.object(message);
-	if (get(object, 'error')) {
-		logger.error(`${channel}.delegate`, error);
+function broadcast(message) {
+	logger.debug(`${namespace}.service.broadcast`, json.string(message));
+	return Object.values(service.workers).reduce((list, worker) => {
+		list.push({
+			id: worker.id,
+			pid: worker.process.pid,
+			result: worker.send(message)
+		});
+		return list;
+	}, []);
+}
+
+function restart() {
+	for (const id in service.workers) {
+		worker.on('exit', () => {
+			if (!worker.exitedAfterDisconnect) return;
+			logger.info(`${namespace}.exit`, {pid:worker.process.pid, restart:true});
+			service.fork({REPLACED_PROCESS:worker.process.pid});
+		});
+		worker.disconnect();
 	}
-	if (!get(object, 'subscribe')) {
-		logger.info(`${channel}.delegate`, object);
+}
+
+function initializeService() {
+	namespace = `${namespace}.service`;
+	logger.info(`${namespace}.init`, config);
+	logger.meta({master: process.pid});
+
+	service.on('setup', settings =>
+		logger.debug(`${namespace}.setup`, json.string(api(settings))));
+
+	service.on('fork', worker =>
+		logger.debug(`${namespace}.fork`, json.string(api(worker))));
+
+	service.on('online', worker =>
+		logger.debug(`${namespace}.online`, json.string(api(worker))));
+
+	service.on('listening', (worker, address) =>
+		logger.debug(`${namespace}.listening`,
+			json.string(Object.assign({worker: worker.id}, address))));
+
+	service.on('disconnect', worker =>
+		logger.debug(`${namespace}.disconnect`, json.string(api(worker))));
+
+	service.on('message', (worker, message) => {
+		logger.info(`${namespace}.receive`, json.string(message));
+	});
+
+	service.on('exit', (worker, code, signal) => {
+		logger.warn(`${namespace}.exit`, json.string(api(worker)));
+		if (code !== 0 && !worker.exitedAfterDisconnect) {
+			service.fork();
+		}
+	});
+
+	process.on('SIGUSR2', () => {
+		logger.info(`${namespace}.restart`, {signal: 'SIGUSR2'});
+		restart();
+	});
+
+	for (let i = 0; i < constant.CPU_COUNT; i++) {
+		service.fork();
 	}
 }
 
-function guard(route) {
-	return route.secure
-	  ? passport.authenticate('jwt-access')
-	  : (request, response, next) => next();
+
+function randomKill() {
+	const random = Math.random() * 10000;
+	setTimeout(() => service.worker.kill(9), random);
+}
+
+function initializeWorker() {
+	namespace = `${namespace}.worker-${service.worker.id}`;
+	logger.meta({worker: process.pid});
+	logger.debug(`${namespace}.init`, {pid: process.pid});
+
+	service.worker.on('error', error => {
+		logger.error(`${namespace}.error`, {failure: error.stack});
+	});
+
+	service.worker.on('exit', (code, signal) => {
+		logger.warn(`${namespace}.exit`, json.string({code, signal}));
+	});
+
+	service.worker.on('message', message => {
+		logger.info(`${namespace}.receive`, json.string(message));
+	});
+
+	worker(/*{...}*/);
 }
 
 
-const service = router();
-
-service.use(passport.initialize());
-
-discover(`${__dirname}/middleware`)
-.forEach(middleware => service.use(require(middleware.module)));
-
-discover(`${__dirname}/route`)
-.forEach(route => service.use(`/${route.name}`, guard(route), require(route.module)));
-
-service.use('/', guard({secure: true}), catchall);
-
-//Array(constant.CPU_COUNT).fill(null, 0, constant.CPU_COUNT)
-//.map((x, i) => {
-//	const port = i + constant.EXPRESS_PORT;
-	const port = constant.EXPRESS_PORT;
-	service.listen(port, () => {
-		logger.info(`${namespace}.listen`, {host, port});
-	})
-//});
-
-//pubsub.subscribe(namespace, delegate);
+if (service.isMaster) {
+	initializeService();
+} else {
+	initializeWorker();
+}
