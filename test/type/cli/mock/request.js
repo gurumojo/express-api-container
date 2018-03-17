a83d70a75d6a9fa4566951ec60a242bf54ee23ee
Matthew Larson <theguy@gurumojo.net>
2018-03-17 01:35:14 -0700

    unit test handler/get-user

--- test/type/cli/mock/request.js
+++ test/type/cli/mock/request.js
@@ -4,9 +4,13 @@ function mockRequest(sandbox) {
 	return {
 		data: {
 			any: sandbox.stub(),
+			one: sandbox.stub(),
 			query: {
 				getUser: sandbox.stub()
 			}
+		},
+		params: {
+			entityID: '00000000-0000-0000-0000-000000000000'
 		}
 	};
 }
